fb5c73abf0dd8c5a5f677b2a769126ba3682aff5
styx <styx>
2022-03-08 11:46:19 +0200

    added ep setup

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
-eb init udagram-app --platform node.js --region us-east-1
+eb init udagram-app --platform "arn:aws:elasticbeanstalk:us-east-1::platform/Node.js 16 running on 64bit Amazon Linux 2/5.5.0"
 eb create --sample udagram-env
 eb setenv POSTGRES_USERNAME="postgres" POSTGRES_PASSWORD="5feeling" POSTGRES_DB="postgres" PORT_DB="5432" POSTGRES_HOST="database-2.cyqd2cjlcqmu.us-east-1.rds.amazonaws.com" AWS_REGION="us-east-1" AWS_BUCKET="arn:aws:s3:::my-284073646289-bucket" JWT_SECRET="this is a secret"
 eb deploy
